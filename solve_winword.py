01329ea7c0c7b0a69835617344aad61ac8006748
Jonghyuk Song <jonghyk.song@gmail.com>
2019-07-23 17:22:08 +0900

    add printStack

--- solve_winword.py
+++ solve_winword.py
@@ -14,7 +14,7 @@ class ESC:
     def __init__(self, dumpPath, initaddr):
         self.dmp = WinDump.WinDump(dumpPath)
         self.emul = Uc(UC_ARCH_X86, UC_MODE_32)
-        self.emul.mem_map(initaddr, 0x14000000)
+        self.emul.mem_map(initaddr, 0x20000000)
 
     def setFS(self, addr):
         self.FS = addr
@@ -36,31 +36,45 @@ class ESC:
         self.emul.reg_write(UC_X86_REG_ESP, 0x130e105e)
         self.emul.reg_write(UC_X86_REG_ESI, 0x0)
         self.emul.reg_write(UC_X86_REG_EDI, 0x0052e418)
-        print("hi")
-        self.emul.reg_write(UC_X86_REG_FS, 0x10000)
+        print("1")
+        #self.emul.reg_write(UC_X86_REG_FS, 0x10000)
+        print("2")
 
     def initMem(self, addr, size):
         data = self.dmp.readMem(addr, size)
         self.emul.mem_write(addr, data)
 
+    def printStack(self, size=0x40):
+        esp = self.emul.reg_read(UC_X86_REG_ESP)
+        stack = self.emul.mem_read(esp, 0x40)
+        stack = binascii.hexlify(stack)
+        print(stack)
+
 
 def main():
     esc = ESC("~/study/WINWORD3.DMP", 0x00010000)
-    print(1)
+    print("load DMP done")
 
     #init registers
     esc.initRegs()
+    print("init regs done")
     
     #init mem
     esc.initMem(0x130e0000, 0x1000)
     #esc.initMem(0x7ffde000, 0x1000)
+    print("init mem done")
 
     #setShellCode
     esc.setShellCode(0x130e3000, b"\x60")
     #esc.setShellCode(0x130e3000, b"\x60\x64\xa1\x00\x00\x00\x00")
+    print("set shellcode done")
 
+    esc.printStack()
+    print("GO")
     #start
     esc.emul.emu_start(esc.shellcode_addr, esc.shellcode_addr+len(esc.shellcode))
+    print("DONE")
+    esc.printStack()
 
 
 
