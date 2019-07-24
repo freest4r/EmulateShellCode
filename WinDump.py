c390836cd36a35a5e66a7625139d4e2f64829432
Jonghyuk Song <jonghyk.song@gmail.com>
2019-07-24 17:21:05 +0900

    using re

--- WinDump.py
+++ WinDump.py
@@ -9,10 +9,15 @@ class WinDump:
         hexstr = ''
         p = os.popen("minidump -r "+str(addr)+" -s "+str(size)+" "+self.dump)
         data = p.read()
+        for m in re.finditer("([0-9a-f]){2} ", data):
+            hexstr += m.group()
+        hexstr = hexstr.replace(" ", "")
+        '''
         data = data.split("\n")
         for i in range(len(data)):
             line = data[i].split()
             for j in range(1, len(line)-1):
                 hexstr += line[j]
+        '''
         return hexstr
 
