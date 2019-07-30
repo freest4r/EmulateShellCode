1ed75f9a2d812f0b94d3ef5f1f8619615609261e
Jonghyuk Song <jonghyk.song@gmail.com>
2019-07-30 09:27:35 +0900

    remove regular exp

--- WinDump.py
+++ WinDump.py
@@ -1,6 +1,4 @@
-import os, re
-
-
+import os
 class WinDump:
     def __init__(self, dumpPath):
         self.dump = dumpPath
@@ -9,15 +7,12 @@ class WinDump:
         hexstr = ''
         p = os.popen("minidump -r "+str(addr)+" -s "+str(size)+" "+self.dump)
         data = p.read()
-        for m in re.finditer("([0-9a-f]){2} ", data):
-            hexstr += m.group()
-        hexstr = hexstr.replace(" ", "")
-        '''
+
         data = data.split("\n")
         for i in range(len(data)):
-            line = data[i].split()
-            for j in range(1, len(line)-1):
+            line = data[i]
+            line = line[18:67].split(" ")
+            for j in range(len(line)):
                 hexstr += line[j]
-        '''
         return hexstr
 
